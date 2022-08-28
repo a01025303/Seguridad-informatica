9a8d148ee57ab7b6a4e31b95905f1c5d5fe9b0a3
Akemi <akemi@Akemis-MacBook-Air.local>
2022-08-27 19:56:58 -0500

    Caesar's cipher

--- Security/caesar.py
+++ Security/caesar.py
@@ -17,20 +17,34 @@ def caesarCipher(keyLetter, message):
     # Initialize variables
     index = 0 # will determine the index of the Key to rotate alphabet 
     encryptedMessage = '' # final result of the encryption, starts empty
+    # For that iterates from 0 to 26 (number of letters in our alphabet)
     for i in range (0,27):
+        # When the alphabet letter matches the key
         if alphabet[i] == keyLetter:
+            # Save index
             index = i
             break
-    arrayTemp1 = alphabet[index:28]
+    # Array that saves the letters from index to letter with index 26
+    arrayTemp1 = alphabet[index:27]
+    # Array that saves the letters from 0 to index - 1
     arrayTemp2 = alphabet[0:index]
+    # Rotated array: merges previous arrays to rotate the whole alphabet
     rotateArray = arrayTemp1 + arrayTemp2
+
+    # Code to translate message into encrypted message
+    # Cicle that iterates through every char in message
     for x in message:
+        # Cicle that, for each char in message, looks for match in alphabet
         for y in range (0,27):
+            # When the char matches a char in alphabet
             if x == alphabet[y]:
+                # Update message, adding a letter from the rotated 
+                # array with the corresponding index
                 encryptedMessage = encryptedMessage + rotateArray[y]
     print(alphabet)
     print(rotateArray)
+    # Return result
     return encryptedMessage
     
 
-print(caesarCipher('t', 'soy bien cool'))
+print(caesarCipher('a', 'soy bien cool'))
